51aa934b1773896c993ed86d02e00b87b12b3be0
Santhosh Kumar <2k.santhosh@gmail.com>
2025-12-25 19:35:11 -0500

    data rendering client vs runtime

---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import AddToCart from '../AddToCart'
+
+
+
+
+const ProductCard
+ = () => {
+  return (
+    <div> 
+        <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
